57a7d0d4670173190621d1d3b992193b698eb8cf
iosif <solomoniosif@gmail.com>
2023-03-10 20:15:36 +0200

    fixed url patterns for expense and transfer edit

--- allocatr/wallet/urls.py
+++ allocatr/wallet/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "wallet"
 
 urlpatterns = [
-    # allocatr Home
+    # Home
     path("", views.DashboardHome.as_view(), name="home"),
+
     # Transaction Views
     path("htmx/transactions/", views.transactions, name="transaction_list"),
     path(
@@ -16,14 +17,15 @@ urlpatterns = [
     path("htmx/add-income/", views.add_income, name="add_income"),
     path("htmx/edit-income/<int:pk>/", views.edit_income, name="edit_income"),
     path("htmx/add-expense/", views.add_expense, name="add_expense"),
-    path("htmx/edit-expense/", views.edit_expense, name="edit_expense"),
+    path("htmx/edit-expense/<int:pk>/", views.edit_expense, name="edit_expense"),
     path("htmx/add-transfer/", views.add_transfer, name="add_transfer"),
-    path("htmx/edit-transfer/", views.edit_transfer, name="edit_transfer"),
+    path("htmx/edit-transfer/<int:pk>/", views.edit_transfer, name="edit_transfer"),
     path(
         "htmx/delete-transaction/<int:pk>/",
         views.delete_transaction,
         name="delete_transaction",
     ),
+
     # Account Views
     path("wallet-accounts/", views.Accounts.as_view(), name="accounts"),
     path("htmx/accounts/", views.account_cards, name="accounts_list"),
