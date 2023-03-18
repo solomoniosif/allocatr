5f8e348b8e4408fc714a240249e6cca5576323bd
iosif <solomoniosif@gmail.com>
2023-03-18 11:35:30 +0200

    replaced Django inserted current period text with data retrieved and processed by Alpine

--- allocatr/static/js/init-alpine.js
+++ allocatr/static/js/init-alpine.js
@@ -61,5 +61,44 @@ function data() {
       this.isAddMenuOpen = false
       this.openModal()
     },
+
+    // Period
+    firstDay: "",
+    lastDay: "",
+    period: {},
+    periodDisplay: "",
+    displayDate(isoStringDate) {
+      const date = new Date(isoStringDate);
+      const language = document.documentElement.lang;
+      let shortMonthName = date.toLocaleString(language, { month: "short" });
+      shortMonthName = shortMonthName.replace(/\./g, "");
+      const day = date.toLocaleString(language, { day: "numeric" });
+
+      // Only return the year if the difference in months between input date and now is more than or equal to 11
+      const now = new Date();
+      let diffMonth = (now.getTime() - date.getTime()) / 1000;
+      diffMonth /= (60 * 60 * 24 * 7 * 4);
+      diffMonth = Math.abs(Math.round(diffMonth))
+      if (diffMonth < 11) {
+        return `${day} ${shortMonthName}`
+      } else {
+        const fullYear = date.getFullYear();
+        const shortYear = fullYear.toString().slice(-2);
+        return `${day} ${shortMonthName} ${shortYear}`
+      }
+    },
+    getPeriodDisplay(firstDay, lastDay) {
+      const firstDayDisplay = this.displayDate(firstDay);
+      const lastDayDisplay = this.displayDate(lastDay);
+      return `${firstDayDisplay} - ${lastDayDisplay}`
+    },
+    getHtmxPeriodVals() {
+      // {"firstPeriodDay": "2023-03-15", "lastPeriodDay": "2023-04-14" }
+      const obj = {
+        firstPeriodDay: this.period.firstDay,
+        lastPeriodDay: this.period.lastDay
+      }
+      return JSON.stringify(obj)
+    },
   }
 }
